dc5ad8bb755a75d52fb730cb6fd24b5e86a25fcb
Hridoy <hriodykumarshil12@gmail.com>
2023-10-19 00:22:12 +0600

    Installation and setup django REST API Project
    https://github.com/Fahad-CSE16/MyRESTAPITutorial

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "firstapp",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = "myproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -104,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+
+TIME_ZONE = "Asia/Dhaka"
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -113,12 +116,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = "/static/"
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-STATIC_URL = "static/"
+# MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
