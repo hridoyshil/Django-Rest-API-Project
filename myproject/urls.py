d49ba3d51d224d9fd7a2509d73d45d590ee8df6d
Hridoy <hriodykumarshil12@gmail.com>
2023-10-20 12:27:41 +0600

    Simple JWT(JSON Web Token)  based Login API and Customization | Increase Lifetime of Token Generated by SimpleJWT
    pip install djangorestframework-simplejwt
    
    python -m pip freeze > requirements.txt
    
    https://www.youtube.com/watch?v=uZNzSx7-m0g&list=PL4NIq30KvXLDES6CUeAWiSJNQzPsoBWI6&index=12

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,11 +5,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from firstapp.views import homeView
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", homeView),
     path("api/login-api/", obtain_auth_token),
+    path("api/token/", MyTokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
     path("api/", include("rest_framework.urls")),
     path("api/firstapp/", include("firstapp.urls")),
 ]
