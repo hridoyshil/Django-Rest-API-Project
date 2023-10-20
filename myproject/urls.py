378eea9c58c64a10407926765e23470a905c718c
Hridoy <hriodykumarshil12@gmail.com>
2023-10-20 11:52:33 +0600

    Token Based Login API | Django REST login API for token based authentication
    https://www.youtube.com/watch?v=R_EcXZzzeI4&list=PL4NIq30KvXLDES6CUeAWiSJNQzPsoBWI6&index=11

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,10 +4,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from firstapp.views import homeView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", homeView),
+    path("api/login-api/", obtain_auth_token),
     path("api/", include("rest_framework.urls")),
     path("api/firstapp/", include("firstapp.urls")),
 ]
